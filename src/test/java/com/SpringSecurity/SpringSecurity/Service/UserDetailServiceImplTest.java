57dd7441b98aad9c3e5359f03d8272bf628bbf37
Bhupal <bhupalprajapati12@gmail.com>
2025-06-14 16:45:05 +0530

    MockitoTest

--- src/test/java/com/SpringSecurity/SpringSecurity/Service/UserDetailServiceImplTest.java
+++ src/test/java/com/SpringSecurity/SpringSecurity/Service/UserDetailServiceImplTest.java
@@ -3,39 +3,46 @@ package com.SpringSecurity.SpringSecurity.Service;
 import com.SpringSecurity.SpringSecurity.Entity.UserEntity;
 import com.SpringSecurity.SpringSecurity.JournalEntryRepository.UserRepo;
 import com.SpringSecurity.SpringSecurity.service.UserDetailServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
+
 public class UserDetailServiceImplTest {
 
-    @Autowired
+    @InjectMocks
     private UserDetailServiceImpl userDetailService;
 
-    @MockitoBean
+    @Mock
     private UserRepo userRepo;
 
+    @BeforeEach
+    void testSetup(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+
     @Test
-    void findbyUserNameTest(){
+    void findbyUserNameTest() {
         UserEntity user = new UserEntity();
         user.setUsername("Name");
         user.setPassword("Name");
-        user.setRoles((List<String>) Collections.singleton("USER"));
+        user.setRoles(Collections.singletonList("USER"));
 
 
         when(userRepo.findByUsername("Name")).thenReturn(Optional.of(user));
 
         assertNotNull(userDetailService);
-        assertEquals("Name",user.getUsername());
-        assertEquals("Name",user.getPassword());
+        assertEquals("Name", user.getUsername());
+        assertEquals("Name", user.getPassword());
     }
 }
